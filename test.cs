e1b6947d5e739a9ba45673641dfa2b99b96a0f4e
ptizrootofevil <148359768+ptizrootofevil@users.noreply.github.com>
2023-11-21 10:20:39 +0300

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,12 @@
+using System;
+
+static internal class Program {
+    static void Main() {
+        if (Math.Abs(Surface.count_surface(6) - 113.09733) <= 0.001) Console.WriteLine("test circle passed");
+
+        if (Math.Abs(Surface.count_surface(3, 4, 5) - 6) <= 0.001) Console.WriteLine("test right triangle passed");
+
+        if (Math.Abs(Surface.count_surface(7, 9, 12) - 31,30495) <= 0.001) Console.WriteLine("test random triangle passed");
+    }
+}
+
